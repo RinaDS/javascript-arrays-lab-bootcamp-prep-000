ea0e7b0b47b4ebffc22edfcf3b19c25c47d8d38b
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-08-04 18:29:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
 function destructivelyAppendKitten(array, element){
   var kitten = ["Milo", "Otis", "Garfield"];
-    array.push(kitten)
+  array.push(kitten)
   return array
 }
