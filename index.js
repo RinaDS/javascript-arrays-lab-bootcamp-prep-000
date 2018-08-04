7d4d778b6a93254103594662603c7041e7cd1753
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-08-04 19:01:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ function prependKitten(array, element){
   return ["Arnold",...kittens];
 }
 function removeLastKitten (array){
-  return kittens.slice(0, array.length - 1);
+  return kittens.slice(0, array.length 1);
 }
